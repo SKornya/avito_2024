5c5ab7655731d0d2504e57208878f2f6e27c8ff4
skornya <kornyakovS@gmail.com>
2024-04-12 13:07:26 +0300

    routes

---
+++ src/routes/index.ts
@@ -0,0 +1,11 @@
+export const BASE_URL = 'https://api.kinopoisk.dev/v1.4';
+
+export const routes = {
+  app: {
+    main: '/',
+    film: '/film/:id',
+  },
+  api: {
+    films: '/movie'
+  },
+};
